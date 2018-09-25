9186d570f49f45858a7f8c3a83a4499e28207093
Connor Wiseman <connorwiseman@familysearch.org>
2018-09-25 10:22:45 -0600

    Added missing JSDoc comments

--- src/router/match.js
+++ src/router/match.js
@@ -7,4 +7,12 @@ const has    = require('../object/has')
 const reduce = require('../array/reduce')
 const split  = require('../url/split')
 
+/**
+ * Given a URL request path {p} and a tree Object {t}, uses the slash-separated
+ * pieces of the path to walk down the tree, returning the Object at the end of
+ * the path or null if not found.
+ * @param  {String} p
+ * @param  {Object} t
+ * @return {Object|null}
+ */
 module.exports = (p, t) => reduce(split(p), (r, c) => r && has(r, c) ? r[c] : null, t)
