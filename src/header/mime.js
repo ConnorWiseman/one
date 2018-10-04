7e1abeb62d5dc8da1b91e970955f3ef184ecd063
Connor Wiseman <wiseman.connor@gmail.com>
2018-10-04 15:08:59 -0600

    Added comments for mime-type sniffing

--- src/header/mime.js
+++ src/header/mime.js
@@ -7,4 +7,10 @@ const mime = require('mime/lite')
 
 const set = require('./set')
 
+/**
+ * Sets the Content-Type header for a given file path, {p}. Not a pure function.
+ * @param  {ServerResponse} res
+ * @param  {String}         p
+ * @return {ServerResponse}
+ */
 module.exports = (res, p) => set(res, 'Content-Type', mime.getType(p))
